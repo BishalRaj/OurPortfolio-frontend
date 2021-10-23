929c2966df62d5112430b582bca7f9c4d378e925
BishalRaj <bisal1shakya@gmail.com>
2021-10-23 21:14:42 +0545

    bootstrap

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Col, Container, Row } from "reactstrap";
+import Card from "@mui/material/Card";
+import { CardContent, Typography } from "@mui/material";
 const Login = () => {
-  return <div>In login</div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg={4} md={4} sm={4}>
+          <Card>
+            <CardContent>
+              <Typography>Hello</Typography>
+            </CardContent>
+          </Card>
+        </Col>
+        <Col lg={8} md={8} sm={8}>
+          form
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
