0ad33e3c39686ddecab711b25a7ee678a98f8d84
BishalRaj <bisal1shakya@gmail.com>
2021-10-24 19:46:43 +0545

    responsive

--- src/layout/authLayout.jsx
+++ src/layout/authLayout.jsx
@@ -27,32 +27,23 @@ const AuthLayout = ({
     },
   };
   return (
-    // <Container fluid style={{ height: "100vh" }}>
-    //   <Row className="p-1 my-0" style={{ height: "inherit" }}>
-    //     <Col lg={4} md={5} sm={12}>
-    //       <Card sx={{ height: "100%" }} className="shadow">
-    //         <CardContent>
-    //           <Typography>{img}</Typography>
-    //         </CardContent>
-    //       </Card>
-    //     </Col>
-    //     <Col lg={8} md={7} sm={12} className="h-100">
-    //       <Row className="py-3 m-0" style={{ height: "4%" }}>
-    //         <Col>{redirectComponent}</Col>
-    //       </Row>
-    //       <Row className="m-0 p-0" style={{ height: "96%" }}>
-    //         <Col className="d-flex align-items-center justify-content-center">
-    //           {formComponent}
-    //         </Col>
-    //       </Row>
-    //     </Col>
-    //   </Row>
-    // </Container>
     <Box sx={{ flexGrow: 1, height: "100vh" }} className="p-3">
       <Grid container sx={{ height: "100%" }}>
-        <Grid item xs={12} md={4}>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{
+            display: "block",
+            "@media (max-width:769px)": { display: "none" },
+          }}
+        >
           <Card
-            sx={{ height: "100%", borderRadius: "2.5%", width: cardWidth }}
+            sx={{
+              height: "100%",
+              borderRadius: "2.5%",
+              width: cardWidth,
+            }}
             className="shadow "
           >
             <Stack className="h-100 px-3">
